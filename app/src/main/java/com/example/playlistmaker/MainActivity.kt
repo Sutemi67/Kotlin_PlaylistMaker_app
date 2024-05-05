02d732e2de0a52b56b87a0419a6c2e37b69aaf3d
Sergey Boykov <sutemi67@gmail.com>
2024-05-05 20:10:11 +0700

    gradle rebuild

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       enableEdgeToEdge()
+      setContentView(R.layout.lesson_images)
       setContentView(R.layout.activity_main)
-      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-         insets
+      
+      val image = findViewById<ImageView>(R.id.poster)
+      
+      val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+         override fun onClick(v: View?) {
+            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+         }
       }
+      
+      image.setOnClickListener(imageClickListener)
    }
 }
