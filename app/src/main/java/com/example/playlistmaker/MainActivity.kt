a50ab30e125b434da3a25f16e72d782aa09a27f7
Sergey Boykov <sutemi67@gmail.com>
2024-05-07 16:01:00 +0700

    gradle rebuild

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       enableEdgeToEdge()
       setContentView(R.layout.lesson_images)
-      setContentView(R.layout.activity_main)
-      
-      val image = findViewById<ImageView>(R.id.poster)
-      
-      val imageClickListener: View.OnClickListener = object : View.OnClickListener {
-         override fun onClick(v: View?) {
-            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
-         }
+      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
+         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+         insets
       }
-      
-      image.setOnClickListener(imageClickListener)
+      val image = findViewById<ImageView>(R.id.poster)
+      image.setOnClickListener{Toast.makeText(this, "You clicked the image", Toast.LENGTH_SHORT).show()}
    }
 }
