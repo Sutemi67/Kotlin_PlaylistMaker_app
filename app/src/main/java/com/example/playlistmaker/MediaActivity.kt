9c93cbf461fae911ae616fff5554d1d9353991ec
sergeyboykov <sutemi67@gmail.com>
2024-06-04 12:57:03 +0300

    Glide imported ready for homework

--- app/src/main/java/com/example/playlistmaker/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaActivity.kt
@@ -1,20 +1,31 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class MediaActivity : AppCompatActivity() {
-   override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
-      enableEdgeToEdge()
-      setContentView(R.layout.activity_media)
-      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-         insets
-      }
-   }
+    private val imageUrl =
+        "https://cs7.pikabu.ru/post_img/2019/09/27/10/1569602772183189348.jpg"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_media)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val image = findViewById<ImageView>(R.id.man)
+        Glide.with(applicationContext)
+            .load(imageUrl)
+            .transform(RoundedCorners(10))
+            .into(image)
+    }
 }
