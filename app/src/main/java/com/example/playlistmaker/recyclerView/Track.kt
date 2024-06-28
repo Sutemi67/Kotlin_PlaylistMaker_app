cd9acffdff159b07771265ee45f2e56ecfe8ef3d
sergeyboykov <sutemi67@gmail.com>
2024-06-28 12:41:41 +0300

    time corrected

--- app/src/main/java/com/example/playlistmaker/recyclerView/Track.kt
+++ app/src/main/java/com/example/playlistmaker/recyclerView/Track.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class Track(
     @SerializedName("trackName") val trackName: String,
     @SerializedName("artistName") val artistName: String,
-    @SerializedName("trackTimeMillis") val trackTime: String,
-    @SerializedName("artworkUrl100") val artworkUrl100:String
+    @SerializedName("trackTimeMillis") val trackTime: Int,
+    @SerializedName("artworkUrl100") val artworkUrl100: String
 )
--- app/src/main/java/com/example/playlistmaker/recyclerView/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/recyclerView/TrackViewHolder.kt
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName: TextView = itemView.findViewById(R.id.track_name)
@@ -17,7 +19,7 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(model: Track) {
         trackName.text = model.trackName
         trackArtist.text = model.artistName
-        trackTime.text = model.trackTime
+        trackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTime)
         Glide.with(itemView)
             .load(model.artworkUrl100)
             .centerCrop()
