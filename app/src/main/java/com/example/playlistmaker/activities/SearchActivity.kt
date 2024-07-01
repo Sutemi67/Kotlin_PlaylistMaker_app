eacd71f73551ea45c37141e270ef2a3c5c95d12b
sergeyboykov <sutemi67@gmail.com>
2024-07-01 09:00:13 +0300

    all errors fixed

--- app/src/main/java/com/example/playlistmaker/activities/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/activities/SearchActivity.kt
@@ -11,7 +11,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
