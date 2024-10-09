a8dcb48c95bc76f58629d6585bd227370a4d4663
Sergey Boykov <sutemi67@gmail.com>
2024-10-09 16:30:14 +0300

    tabs are created

--- app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
@@ -5,18 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityMediaBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMediaBinding
+    private lateinit var tabLayoutMediator: TabLayoutMediator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_media)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMediaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.pagerLayout.adapter = FragmentsAdapter(supportFragmentManager, lifecycle)
+        tabLayoutMediator =
+            TabLayoutMediator(binding.tableLayout, binding.pagerLayout) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "Favourites"
+                    1 -> tab.text = "Playlists"
+                }
+            }
+        tabLayoutMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabLayoutMediator.detach()
     }
 }
