de0aa76bd8f1f68e4a3115d84f1b705df6cbd68b
Sergey Boykov <sutemi67@gmail.com>
2024-10-18 00:40:17 +0300

    and something else deleted

--- app/src/main/java/com/example/playlistmaker/di/AppModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/AppModule.kt
@@ -1,7 +1,6 @@
 package com.example.playlistmaker.di
 
 import com.example.playlistmaker.main.ui.SingleActivityViewModel
-import com.example.playlistmaker.media.ui.FavouritesViewModel
 import com.example.playlistmaker.player.ui.PlayerViewModel
 import com.example.playlistmaker.search.ui.FragmentSingleSearchViewModel
 import com.example.playlistmaker.settings.ui.FragmentSettingsViewModel
@@ -10,7 +9,6 @@ import org.koin.dsl.module
 
 val appModule = module {
     viewModel<PlayerViewModel> { PlayerViewModel(get(), get()) }
-    viewModel<FavouritesViewModel> { FavouritesViewModel() }
     viewModel<FragmentSettingsViewModel> { FragmentSettingsViewModel(get()) }
     viewModel<FragmentSingleSearchViewModel> { FragmentSingleSearchViewModel(get()) }
     viewModel<SingleActivityViewModel> { SingleActivityViewModel(get()) }
--- app/src/main/java/com/example/playlistmaker/media/ui/FragmentSingleMedia.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/FragmentSingleMedia.kt
@@ -8,25 +8,11 @@ import androidx.fragment.app.Fragment
 import com.example.playlistmaker.databinding.FragmentSingleMediaBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-
-
 class FragmentSingleMedia : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var binding: FragmentSingleMediaBinding
     private lateinit var tabMediator: TabLayoutMediator
     private lateinit var tabAdapter: MediaViewPagerAdapter
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,15 +36,13 @@ class FragmentSingleMedia : Fragment() {
         tabMediator.attach()
     }
 
-    companion object {
+//    companion object {
 
 //        @JvmStatic
 //        fun newInstance(param1: String, param2: String) =
 //            FragmentSingleMedia().apply {
 //                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
 //                }
 //            }
-    }
+//    }
 }
