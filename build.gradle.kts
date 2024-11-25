a472978776e0ba32310f44afb958b9689cdd0767
Sergey Boykov <sutemi67@gmail.com>
2024-11-25 13:03:04 +0300

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
    alias(libs.plugins.android.application) apply false
    alias(libs.plugins.jetbrains.kotlin.android) apply false
+   id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+
 }
