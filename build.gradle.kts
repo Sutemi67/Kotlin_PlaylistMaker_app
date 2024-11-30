c4e12eac5d3924c1982e90debb11895f941774bb
Sergey Boykov <sutemi67@gmail.com>
2024-11-30 10:28:13 +0300

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
    alias(libs.plugins.android.application) apply false
    alias(libs.plugins.jetbrains.kotlin.android) apply false
-   id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+   id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 
 }
