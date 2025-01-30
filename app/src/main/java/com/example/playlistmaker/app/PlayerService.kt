30a22866301fc37afe1866fc3310bba7bfd3e9a8
sutemi_pc <sutemi67@gmail.com>
2025-01-30 20:39:20 +0300

    service created

--- app/src/main/java/com/example/playlistmaker/app/PlayerService.kt
+++ app/src/main/java/com/example/playlistmaker/app/PlayerService.kt
@@ -2,12 +2,72 @@ package com.example.playlistmaker.app
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 
 class PlayerService : Service() {
 
+    companion object {
+        private const val LOG_TAG = "MusicService"
+    }
 
-    override fun onBind(p0: Intent?): IBinder? {
-        return null
+    private var mediaPlayer: MediaPlayer? = null
+    private var songUrl = ""
+    private val binder = MusicServiceBinder()
+
+    inner class MusicServiceBinder : Binder() {
+        fun getService(): PlayerService = this@PlayerService
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer()
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        songUrl = intent?.getStringExtra("song_url") ?: ""
+        initMediaPlayer()
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(p0: Intent?): IBinder {
+        return binder
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
+    private fun initMediaPlayer() {
+        if (songUrl.isEmpty()) return
+
+        mediaPlayer?.setDataSource(songUrl)
+        mediaPlayer?.prepareAsync()
+        mediaPlayer?.setOnPreparedListener {
+            Log.d(LOG_TAG, "Media Player prepared")
+        }
+        mediaPlayer?.setOnCompletionListener {
+            Log.d(LOG_TAG, "Playback completed")
+        }
+    }
+
+    fun startPlayer() {
+        mediaPlayer?.start()
+    }
+
+    fun pausePlayer() {
+        mediaPlayer?.pause()
+    }
+
+    private fun releasePlayer() {
+        mediaPlayer?.stop()
+        mediaPlayer?.setOnPreparedListener(null)
+        mediaPlayer?.setOnCompletionListener(null)
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
